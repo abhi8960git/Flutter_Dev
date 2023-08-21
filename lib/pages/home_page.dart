2a8fd8018342a4433f00f28f8aa2cc06b04635c6
Abhishek <21bcs116@nith.ac.in>
2023-08-22 01:14:08 +0530

    Complete Grid View and UI refactoring:art:

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -50,15 +50,64 @@ class _HomePageState extends State<HomePage> {
       drawer: MyDrawer(),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: (HashiraModel.hashiras != null && HashiraModel.hashiras.isNotEmpty) ? ListView.builder(
-          itemCount: HashiraModel.hashiras.length,
+        child: (HashiraModel.hashiras != null && HashiraModel.hashiras.isNotEmpty) 
+        ? GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 1),
           itemBuilder: (context, index) {
-            return HashiraWidget(
-              item: HashiraModel.hashiras[index],
+            final item = HashiraModel.hashiras[index];
+            return Card(
+              clipBehavior: Clip.antiAlias,
+
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+              child: GridTile(
+                child: Image.network(item.image_url),
+                
+                header: Container(
+                  child: Center(
+                    child: Text(item.name,style: TextStyle(fontSize: 24),
+                    
+                  )),
+                  padding:const EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 177, 178, 96),
+                  ),
+
+
+                  ),
+                footer: Container(
+                  child: Center(
+                    child: Text(item.description,
+                    style: TextStyle(fontSize: 25,color: Colors.lightBlue),
+                    
+                    
+                    ),
+                    
+                    
+                  
+                  ),
+                  padding: EdgeInsets.symmetric(vertical: 22),
+                )
+                ),
             );
+          
           },
+          itemCount: HashiraModel.hashiras.length,
         ): Center(child: CircularProgressIndicator(),)
       ),
     );
   }
 }
+
+
+
+
+// In the List View Case we use this : 
+
+//  ListView.builder(
+//           itemCount: HashiraModel.hashiras.length,
+//           itemBuilder: (context, index) {
+//             return HashiraWidget(
+//               item: HashiraModel.hashiras[index],
+//             );
+//           },
+//         )
